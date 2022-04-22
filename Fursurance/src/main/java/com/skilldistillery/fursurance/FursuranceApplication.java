1f7b7f356b297a6e05cc484f4f58d051d2798b07
TaylorY1 <YockeyT139@gmail.com>
2022-04-22 09:59:16 -0600

    Create Spring Boot MVC project, add JPA project dependency

---
+++ Fursurance/src/main/java/com/skilldistillery/fursurance/FursuranceApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fursurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FursuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FursuranceApplication.class, args);
+	}
+
+}
