a60c4dfcf16846d9c1811c81c44991a1dc2aa511
Daniel.Kregstein <Akregstein@hotmail.com>
2022-04-23 11:04:30 -0600

    completed vet entity and j-unit

---
+++ JPAFursurance/src/test/java/com/skilldistillery/fursurance/entities/VetTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.fursurance.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VetTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Vet vet;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		 emf = Persistence.createEntityManagerFactory("JPAFursurance");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em= emf.createEntityManager();
+		vet = em.find(Vet.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		vet = null;
+	}
+
+	@Test
+	void test_initial_vet_entity_mapping() {
+		assertNotNull(vet);
+		assertEquals("harry",vet.getName());
+	}
+
+}
