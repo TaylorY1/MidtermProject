796236fdc14b60bbe34f01919518eb30ba051286
Daniel.Kregstein <Akregstein@hotmail.com>
2022-04-25 16:39:57 -0600

    completed registration controller and impl

--- Fursurance/src/main/java/com/skilldistillery/fursurance/data/UserDAO.java
+++ Fursurance/src/main/java/com/skilldistillery/fursurance/data/UserDAO.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.fursurance.data;
 
+import com.skilldistillery.fursurance.entities.Address;
 import com.skilldistillery.fursurance.entities.User;
 
 public interface UserDAO {
@@ -7,5 +8,9 @@ public interface UserDAO {
 	User findById(int userId);
 
 	User findByCredentials(String username, String password);
+
+	User findByFullName(String firstName, String lastName);
+
+	boolean addUser(User user, Address address);
 
 }
