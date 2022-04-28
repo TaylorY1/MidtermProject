f0b3c93f84ba2ec8e8e1b178ab3f398fdef89709
TaylorY1 <YockeyT139@gmail.com>
2022-04-28 14:00:16 -0600

    Configure app for deployment

--- Fursurance/src/main/java/com/skilldistillery/fursurance/FursuranceApplication.java
+++ Fursurance/src/main/java/com/skilldistillery/fursurance/FursuranceApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.fursurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FursuranceApplication {
+public class FursuranceApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(FursuranceApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FursuranceApplication.class, args);
